079a4aa8f5519bbc2e26d71489a7fb5da0653a78
ASkeTizy <45819392+ASkeTizy@users.noreply.github.com>
2023-12-18 11:05:44 +0300

    Update FizzComponent.java

--- fizzBuzz/FizzComponent.java
+++ fizzBuzz/FizzComponent.java
@@ -2,10 +2,9 @@ package org.example.fizzBuzz;
 
 public class FizzComponent extends Component{
     @Override
-    public boolean handle(int i) {
+    public String handle(int i) {
         if(i % 3 == 0) {
-            System.out.println("Fizz");
-            return  true;
+            return  "Fizz";
         }
         return checkNext(i);
     }
