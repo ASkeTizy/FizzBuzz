a7112aefc9ab82967914069c2fc5277d2ba68177
ASkeTizy <45819392+ASkeTizy@users.noreply.github.com>
2023-12-18 11:06:24 +0300

    Update FizzBuzz.java

--- fizzBuzz/FizzBuzz.java
+++ fizzBuzz/FizzBuzz.java
@@ -1,41 +1,40 @@
 package org.example.fizzBuzz;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 public class FizzBuzz {
-    public Component component;
     public List<String> fizzBuzz(int n) {
         List<String> ans = new LinkedList<>();
-        for (int i = 1; i <= n ; i++) {
-            if (i % 5 == 0 && i % 3 == 0){
+        for (int i = 1; i <= n; i++) {
+            if (i % 5 == 0 && i % 3 == 0) {
                 ans.add("FizzBuzz");
             } else if (i % 3 == 0) {
                 ans.add("Fizz");
             } else if (i % 5 == 0) {
-                    ans.add("Buzz");
-                }   else {
-                    ans.add(i + "");
-                }
+                ans.add("Buzz");
+            } else {
+                ans.add(i + "");
+            }
 
         }
         return ans;
     }
-    public void setComponent(Component component) {
-        this.component = component;
-    }
-    public boolean checker(int i) {
-        return component.handle(i);
-    }
+
+
     public static void main(String[] args) {
         FizzBuzz fizzBuzz = new FizzBuzz();
-        for (int i =1; i < 16; i++) {
-            Component component1 = Component.link(new FizzBuzzComponent(),
-                    new FizzComponent(),
-                    new BuzzComponent());
-            fizzBuzz.setComponent(component1);
-            fizzBuzz.component.handle(i);
+
+        Component component1 = Component.link(
+                new FizzBuzzComponent(),
+                new FizzComponent(),
+                new BuzzComponent());
+        List<String> list = new ArrayList<>();
+        for (int i = 1; i < 16; i++) {
+            list.add(component1.handle(i));
         }
+        list.forEach(el -> System.out.println(el));
 
     }
 }
